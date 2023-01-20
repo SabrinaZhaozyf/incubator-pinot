9cbc539014f3b832b8359ebebae4957fb024727b
SabrinaZhaozyf <yifzhao@linkedin.com>
2023-01-20 15:46:58 -0800

    commit

--- pinot-broker/src/main/java/org/apache/pinot/broker/broker/PinotFSResultStoreFactory.java
+++ pinot-broker/src/main/java/org/apache/pinot/broker/broker/PinotFSResultStoreFactory.java
@@ -1,5 +1,50 @@
 package org.apache.pinot.broker.broker;
 
-public class PinotFSResultStoreFactory extends ResultStoreFactory{
+import org.apache.pinot.broker.api.ResultStore;
+import org.apache.pinot.common.response.BrokerResponse;
+import org.apache.pinot.spi.env.PinotConfiguration;
+import org.apache.pinot.spi.trace.RequestContext;
 
+
+public class PinotFSResultStoreFactory extends ResultStoreFactory {
+
+  private ResultStore _resultStore;
+
+  public PinotFSResultStoreFactory() {
+  }
+
+  @Override
+  public void init(PinotConfiguration configuration) {
+    _resultStore = new PinotFSResultStore();
+  }
+
+  @Override
+  public ResultStore create() {
+    return _resultStore;
+  }
+
+  private static class PinotFSResultStore implements ResultStore {
+
+    @Override
+    public void initPagination(String pointer, RequestContext requestContext)
+        throws Exception {
+
+    }
+
+    @Override
+    public boolean uploadPaginationResult(String pointer, BrokerResponse brokerResponse) {
+      return false;
+    }
+
+    @Override
+    public BrokerResponse getPaginatedResult(String pointer, int offset, int fetch)
+        throws Exception {
+      return null;
+    }
+
+    @Override
+    public boolean deletePaginationResult(String pointer) {
+      return false;
+    }
+  }
 }
--- pinot-broker/src/main/java/org/apache/pinot/broker/broker/ResultStoreFactory.java
+++ pinot-broker/src/main/java/org/apache/pinot/broker/broker/ResultStoreFactory.java
@@ -1,4 +1,27 @@
 package org.apache.pinot.broker.broker;
+import org.apache.pinot.broker.api.ResultStore;
+import org.apache.pinot.spi.env.PinotConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class ResultStoreFactory {
+  public static final Logger LOGGER = LoggerFactory.getLogger(ResultStoreFactory.class);
+  public static final String RESULT_STORE_CLASS_CONFIG = "class";
+
+  public void init(PinotConfiguration configuration) {
+  };
+
+  public abstract ResultStore create();
+
+  public static ResultStoreFactory loadFactory(PinotConfiguration configuration) {
+    String resultStoreFactoryClassName = configuration.getProperty(RESULT_STORE_CLASS_CONFIG);
+    ResultStoreFactory resultStoreFactory;
+    try {
+      resultStoreFactory = (ResultStoreFactory) Class.forName(resultStoreFactoryClassName).newInstance();
+      resultStoreFactory.init(configuration);
+      return resultStoreFactory;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
